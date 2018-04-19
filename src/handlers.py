ca65b9a549deafcd56286cb2c8ded15d4890c7e6
Steve Norum <stevenorum@gmail.com>
2018-04-19 18:06:10 -0400

    Improving logging to help debug stuff.

--- src/handlers.py
+++ src/handlers.py
@@ -1,4 +1,18 @@
 import json
+import logging
+import sys
+
+root = logging.getLogger()
+root.setLevel(logging.DEBUG)
+
+ch = logging.StreamHandler(sys.stdout)
+ch.setLevel(logging.DEBUG)
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+ch.setFormatter(formatter)
+root.addHandler(ch)
+
+for noisy in ('botocore', 'boto3', 'requests'):
+    logging.getLogger(noisy).level = logging.WARN
 
 import api_stuff
 import s3_stuff
