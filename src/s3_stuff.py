23d8aff4ece65b732bdd2a10fead16fa36865551
Steve Norum <stevenorum@gmail.com>
2018-04-19 01:06:05 -0400

    Fixing slight error with the S3 upload catcher.

--- src/s3_stuff.py
+++ src/s3_stuff.py
@@ -9,14 +9,16 @@ def handle_s3_event(event, context):
         try:
             bucket = record["s3"]["bucket"]["name"]
             key = record["s3"]["object"]["key"]
-            image = Image(image_name=key)
-            image.save()
 
             who = record["userIdentity"]["principalId"]
             what = record["eventName"]
             when = record["eventTime"]
             message = "Around {when}, user {who} performed action {what} on object {key} in bucket {bucket}".format(bucket=bucket, key=key, who=who, what=what, when=when)
             print(message)
+
+            image = Image(image_name=key)
+            image._save()
+
         except:
             traceback.print_exc()
             print("Error handling record.")
