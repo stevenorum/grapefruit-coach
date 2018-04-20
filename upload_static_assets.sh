8e0b96e857e104bbd428e3f8989695099f166581
Steve Norum <stevenorum@gmail.com>
2018-04-19 20:09:07 -0400

    Fixing some build issues.

--- upload_static_assets.sh
+++ upload_static_assets.sh
@@ -1,18 +1,15 @@
 #!/bin/bash
 
-# linux: md5sum
-# mac: md5
-export MD5CMD=$(which md5sum)
-which md5sum > /dev/null || export MD5CMD=$(which md5)
-
 hash_errything() {
     find static/* | sort | while read fname ; do
-        cat $fname | $MD5CMD
+        cat $fname | md5sum
     done
 }
 
-CHECKSUM=$(hash_errything | $MD5CMD)
+CHECKSUM=$(hash_errything | md5sum)
 
 aws s3 sync static s3://$STATIC_BUCKET/$CHECKSUM/static
+
+echo "{\"STATIC_BUCKET\":\"$STATIC_BUCKET\",\"STATIC_PATH\":\"$CHECKSUM\"}"
 
 echo "{\"STATIC_BUCKET\":\"$STATIC_BUCKET\",\"STATIC_PATH\":\"$CHECKSUM\"}" > $1
