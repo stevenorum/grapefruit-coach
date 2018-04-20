f47812817f523da1d6df02d0bf56cc67ff1b29b6
stevenorum <stevenorum@gmail.com>
2018-04-19 21:25:56 -0400

    Update upload_static_assets.sh
    Fixing checksum logic to remove non-hex characters.

--- upload_static_assets.sh
+++ upload_static_assets.sh
@@ -2,12 +2,12 @@
 
 hash_errything() {
     find static/* | sort | while read fname ; do
-        cat $fname | md5sum
+        cat $fname | md5sum | grep -o "[0-9a-f]{32}"
     done
 }
 
-CHECKSUM=$(hash_errything | md5sum)
-CHECKSUM=${CHECKSUM%% -}
+CHECKSUM=$(hash_errything | md5sum | grep -o "[0-9a-f]{32}")
+echo "Checksum: '$CHECKSUM'"
 
 aws s3 sync static s3://$STATIC_BUCKET/$CHECKSUM/static
 
